f44389771cde2a23b9c36e67c0bbec04fa85b90a
anamile1 <amarias427@misena.edu.co>
2021-09-03 19:35:05 -0500

    Se sube estructura de archivos en spring del proyecto invent

---
+++ src/main/java/com/Invent/demo/Controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.Invent.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+	
+	@Controller
+	@RequestMapping("/app")
+	public class IndexController {
+		@GetMapping({"/home","/","/index"})
+		public String index(Model model) {
+				model.addAttribute("titulo", "Aprendiendo Spring ADSI");
+				return "index";
+			}
+
+}
